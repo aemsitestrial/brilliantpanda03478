b8c73675612afb52fa70997d17270639fb65d647
vlp-1234 <vangala.priya@tcs.com>
2026-09-22 19:56:26 +0530

    updated codes

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -9,11 +9,21 @@ export default function decorate(block) {
     moveInstrumentation(row, li);
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+      if (div.children.length === 1 && div.querySelector('picture')) {
+        div.className = 'cards-card-image';
+      } else {
+        div.className = 'cards-card-body';
+
+        // Check if a top tag/badge paragraph exists and has content
+        const firstPara = div.querySelector('p:first-child');
+        if (firstPara && !firstPara.querySelector('a') && div.children.length > 1) {
+          firstPara.classList.add('cards-card-badge');
+        }
+      }
     });
     ul.append(li);
   });
+
   ul.querySelectorAll('picture > img').forEach((img) => {
     const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
     moveInstrumentation(img, optimizedPic.querySelector('img'));
